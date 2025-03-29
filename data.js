69db7273e63198ec5797e5d2c506e7354bdc5fae
13golds <13gaudi@gmail.com>
2025-03-30 02:10:38 +0300

    Update data.js

--- data.js
+++ data.js
@@ -1,1 +1,23 @@
+export const tokenMetadata = {
+  name: "Kisik",
+  symbol: "KISIK",
+  description: "Kisik is a meme token built for joy and identity in Web3. Its symbol is a white cat in sunglasses and a floral scarf offering peace, coffee, and curiosity to the world.",
+  image: "https://raw.githubusercontent.com/13golds/MEME-KISIK-INU/main/images/image.png",
+  external_url: "https://13golds.github.io/MEME-KISIK-INU/",
+  properties: {
+    category: "image",
+    creators: [
+      {
+        address: "Gaudi (founded by Gold, guided by Luxor)",
+        share: 100
+      }
+    ],
+    files: [
+      {
+        uri: "https://raw.githubusercontent.com/13golds/MEME-KISIK-INU/main/images/image.png",
+        type: "image/png"
+      }
+    ]
+  }
+};
 
